29e28be86449eae8c9a45bd2de2804829cf2574b
wardrockay <tolliam.e@gmail.com>
2025-02-10 16:57:56 +0100

    add collections

--- InfinityMint-frontend/layouts/default.vue
+++ InfinityMint-frontend/layouts/default.vue
@@ -1,7 +1,48 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Art',
+  icon: 'i-heroicons-book-open',
+  to: '/getting-started'
+}, {
+  label: 'Gaming',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/pro'
+}, {
+  label: 'Photographie',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/releases'
+}]
+</script>
+
+
 <template>
-    <div>
-      <p>Some default layout content shared across all pages</p>
-      <slot />
-    </div>
-  </template>
+  <UHeader :links="links">
+    <template #logo>
+      InfinityMint
+    </template>
+
+    <template #center>
+      <UHeaderLinks :links="links" />
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+      <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray"
+        variant="ghost" />
+    </template>
+
+
+  </UHeader>
+
+  <UPage class="m-8">
+    <UPageBody>
+      
+        <slot />
+
+    </UPageBody>
+  </UPage>
+
+
   
+
+</template>
--- InfinityMint-frontend/pages/index.vue
+++ InfinityMint-frontend/pages/index.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
+
+    <div class="text-xl font-extrabold mb-4">
+        Collections
+    </div>
+
+    <UPageGrid>
+
+        <ULandingCard title="Portfolio"
+            description="Veniam minim ipsum anim. Irure voluptate magna dolore id dolore ex quis sint sint et duis dolor enim fugiat."
+            icon="i-heroicons-photo" color="primary" orientation="horizontal">
+            <img src="https://picsum.photos/640/360?grayscale" class="w-full rounded-md" />
+        </ULandingCard>
+
+        <ULandingCard title="Portfolio"
+            description="Veniam minim ipsum anim. Irure voluptate magna dolore id dolore ex quis sint sint et duis dolor enim fugiat."
+            icon="i-heroicons-photo" color="primary" orientation="horizontal">
+            <img src="https://picsum.photos/640/360?grayscale" class="w-full rounded-md" />
+        </ULandingCard>
+
+        <ULandingCard title="Portfolio"
+            description="Veniam minim ipsum anim. Irure voluptate magna dolore id dolore ex quis sint sint et duis dolor enim fugiat."
+            icon="i-heroicons-photo" color="primary" orientation="horizontal">
+            <img src="https://picsum.photos/640/360?grayscale" class="w-full rounded-md" />
+        </ULandingCard>
+    </UPageGrid>
+
 </template>

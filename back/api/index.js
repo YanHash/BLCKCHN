41b012907d8798f2898547115743cbc367b9fb94
Sylvain Delavacquery <sylvaindelavacquery63@gmail.com>
2025-02-10 17:05:17 +0100

    suppresion credentials

--- back/api/index.js
+++ back/api/index.js
@@ -6,20 +6,6 @@ const { getFirestore, collection, getDocs, doc, setDoc} = require("firebase/fire
 
 const app = express()
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDJfDYmu3P2DYlG24_v1soCY4bwYJfuPU8",
-//     authDomain: "infinitymint.firebaseapp.com",
-//     projectId: "infinitymint",
-//     storageBucket: "infinitymint.firebasestorage.app",
-//     messagingSenderId: "17080244891",
-//     appId: "1:17080244891:web:b490790003d0a0379e843e"
-// };
-  
-// // Initialize Firebase
-// const appFirebase = initializeApp(firebaseConfig);
-// const db = getFirestore(appFirebase);
-
 variables.databaseFirebase = new database()
 variables.databaseFirebase.connexionDatabase()
   
